8a1269e673162a76b4b1946438a43a2a001956bc
Ievgeniia Mykhailenko <myewgeniya@gmail.com>
2022-08-10 11:32:18 +0200

    invoke command

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -78,13 +78,88 @@ it('second test', () => {
 
 })
 
-it.only('then and wrap methods', () => {
+it('then and wrap methods', () => {
 
     cy.visit('/')
     cy.contains('Forms').click()
     cy.contains('Form Layouts').click({force: true})
     
-    cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
-    cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+    // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+    // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+
+    // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+    // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+    
+    // Jquery method
+    cy.contains('nb-card', 'Using the Grid').then( firstForm => {
+        const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+        const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+        // Chai assartions
+        expect(emailLabelFirst).to.equal('Email')
+        expect(passwordLabelFirst).to.equal('Password')
+
+
+        cy.contains('nb-card', 'Basic form').then(secondForm => {
+            const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+            expect (passwordLabelFirst).to.equal(passwordSecondText)
+
+
+            // Convert format from Jquery to cypress syntax use method "wrap"
+            cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+    })
+        
+        
+    })
 })
+
+
+it('invoke command', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click({force: true})
+
+    // 1st approach
+    cy.get('[for="exampleInputEmail1"]').should('contain','Email address')
+
+    // 2nd approach
+    cy.get('[for="exampleInputEmail1"]').then( label => {
+        expect(label.text()).to.equal('Email address')
+
+    // 3rd approach
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+        expect(text).to.equal('Email address')
+
+    cy.contains('nb-card', 'Basic form')
+        .find('nb-checkbox')
+        .click()
+        .find('.custom-checkbox')
+        .invoke('attr', 'class')
+        //.should('contain', 'checked')
+        .then(classValue => {
+            expect(classValue).to.contain('checked')
+        }) 
+        
+
+    })
+    
+    })
+
+})
+
+it.only('assert property', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Datepicker').click({force: true})
+
+    cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+        cy.wrap(input).click()
+        cy.get('nb-calendar-day-picker').contains('17').click()
+        cy.wrap(input).invoke('prop', 'value').should('contain', 'Aug 17, 2022')    
+        
+    })
+
+
+} )
 
